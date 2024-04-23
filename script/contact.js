b1949c94b9d28ee058949c882050f654c53e56bb
Carol-88 <gralleta@gmail.com>
2024-04-23 18:56:37 +0200

    ✨ add contact js

---
+++ script/contact.js
@@ -0,0 +1,38 @@
+document.addEventListener('DOMContentLoaded', function () {
+        const form = document.querySelector('form');
+        form.addEventListener('submit', function (event) {
+          event.preventDefault(); // Previene el envío del formulario
+
+          const fullName = document.getElementById('full-name').value;
+          const email = document.getElementById('email').value;
+          const phone = document.getElementById('phone').value;
+          const message = document.getElementById('message').value;
+
+         
+          if (fullName.toLowerCase() === 'ironhack') {
+            alert('You cannot be Ironhack, because I am Ironhack.');
+            return;
+          }
+
+   
+          const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+          if (!emailRegex.test(email)) {
+            alert('Please enter a valid email address.');
+            return;
+          }
+
+         
+          if (phone && !/^\d{10}$/.test(phone)) {
+            alert('Please enter a valid phone number.');
+            return;
+          }
+
+         
+          if (!fullName || !email || !message) {
+            alert('Please fill in all required fields.');
+            return;
+            }
+            
+          alert('Form submitted successfully!');
+        });
+      });
